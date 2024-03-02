a2b3414fc0dfcfa7f8d063e1e637e0cebace058f
nicoletomazonipelentir <nicole.tpelentir@hotmail.com>
2024-03-02 13:23:15 -0300

    first commit

---
+++ Model.php
@@ -0,0 +1,16 @@
+<?php
+
+
+ class Model{
+    public $string;
+    
+    public function _construct(){
+        $this->string="aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    }
+
+    public function get_string(){
+        return $this->string;
+    }
+
+ }
+
